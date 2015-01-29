1ad4796a1633f2a39ffec3163c3b35d4d31ffc67
mac <mac@mactekiMacBook.local>
2015-01-30 00:21:19 +0800

    12121

---
+++ asdsad/asdsad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  asdsad
+//
+//  Created by mac on 15/1/30.
+//  Copyright (c) 2015年 武汉亦鸟科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ asdsad/asdsad/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  asdsad
+//
+//  Created by mac on 15/1/30.
+//  Copyright (c) 2015年 武汉亦鸟科技有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"sdfsdfs");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
